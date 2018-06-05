5f5f38103a7c9f24b37891f168839d0d57084e1e
bluepython508 <bluepython508@gmail.com>
2018-06-05 15:17:00 +0100

    Fixed format directive

--- organizeSchoolWork.py
+++ organizeSchoolWork.py
@@ -8,10 +8,11 @@ from pathlib import Path
 def new_doc(subject, name):
     template = (Path.home() / "Documents"/ "SchoolWork" / "template.docx").absolute().resolve()
     loc = (Path.home() / "Documents" / "SchoolWork" / subject).absolute().resolve()
+    loc.mkdir(exist_ok=True)
     file = (loc / f"{date.today().isoformat()}-{name}.docx").absolute().resolve()
     doc = Document(os.fspath(template))
     for para in doc.paragraphs:
-        para.text = para.text.format({'date': date.today().strftime("%a, %d %b %Y"), 'heading': name,
-                                      'body': 'Replace this...'})
+        para.text = para.text.format(**{'date': date.today().strftime("%a, %d %b %Y"), 'heading': name,
+                                        'body': 'Replace this...'})
     doc.save(os.fspath(file))
     Popen(["start", os.fspath(file)])
