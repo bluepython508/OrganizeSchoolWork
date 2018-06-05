a74f366b495acb81de3c4c2cfab46edaa93b6d8f
bluepython508 <bluepython508@gmail.com>
2018-06-05 14:41:07 +0100

    Moved to pathlib over ostools

--- organizeSchoolWork.py
+++ organizeSchoolWork.py
@@ -1,20 +1,20 @@
-from ostools import FilePath
 from docx import Document
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT as P_ALIGN
 from datetime import date
 from subprocess import Popen
+import os
+from pathlib import Path
 
 
 def new_doc(subject, name):
-    template = FilePath("%HomeDrive%%HomePath%", ensure="dir").child("Documents").child("SchoolWork")\
-        .getChild("template.docx", ensure="file")
-    loc = FilePath("%HomeDrive%%HomePath%", ensure="dir").child("Documents").child("SchoolWork").child(subject)
-    file = FilePath(loc, ensure="file").child(f"{date.today().isoformat()}-{name}.docx")
-    doc = Document(template.path)
+    template = (Path.home() / "Documents"/ "SchoolWork" / "template.docx").absolute().resolve()
+    loc = (Path.home() / "Documents" / "SchoolWork" / subject).absolute().resolve()
+    file = (loc / f"{date.today().isoformat()}-{name}.docx").absolute().resolve()
+    doc = Document(os.fspath(template))
     date_para = doc.add_paragraph(date.today().strftime("%a, %d %b %Y"), "Heading2")
     date_para.alignment = P_ALIGN.RIGHT
     head = doc.add_paragraph(name, "Title")
     head.alignment = P_ALIGN.CENTER
     body = doc.add_paragraph("This is the body...", "Body")
-    doc.save(file.path)
+    doc.save(os.fspath(file))
     Popen(["start", file.path])
