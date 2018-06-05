89a439ad49cb58d462e1ddfd8d569d6a367715dc
bluepython508 <bluepython508@gmail.com>
2018-06-05 15:29:00 +0100

    Added full cross-platform support

--- organizeSchoolWork.py
+++ organizeSchoolWork.py
@@ -3,11 +3,12 @@ from datetime import date
 from subprocess import Popen
 import os
 from pathlib import Path
+import sys
 
 
 def new_doc(subject, name):
-    template = (Path.home() / "Documents"/ "SchoolWork" / "template.docx").absolute().resolve()
-    loc = (Path.home() / "Documents" / "SchoolWork" / subject).absolute().resolve()
+    template = (Path.home() / "Documents" / "SchoolWork" / "template.docx").absolute().resolve()
+    loc = (Path.home() / "Documents" / "SchoolWork" / subject).absolute()
     loc.mkdir(exist_ok=True)
     file = (loc / f"{date.today().isoformat()}-{name}.docx").absolute().resolve()
     doc = Document(os.fspath(template))
@@ -15,4 +16,10 @@ def new_doc(subject, name):
         para.text = para.text.format(**{'date': date.today().strftime("%a, %d %b %Y"), 'heading': name,
                                         'body': 'Replace this...'})
     doc.save(os.fspath(file))
-    Popen(["start", os.fspath(file)])
+    if sys.platform == 'darwin':
+        utility = 'open'
+    elif sys.platform.startswith('win'):
+        utility = 'start'
+    else:
+        utility = 'xdg-open'
+    Popen([utility, os.fspath(file)])
