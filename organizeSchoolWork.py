e4e9229978fea1c1342b59078057aa5ad3e688f7
bluepython508 <bluepython508@gmail.com>
2018-06-05 15:01:15 +0100

    Changed role of templates

--- organizeSchoolWork.py
+++ organizeSchoolWork.py
@@ -1,5 +1,4 @@
 from docx import Document
-from docx.enum.text import WD_PARAGRAPH_ALIGNMENT as P_ALIGN
 from datetime import date
 from subprocess import Popen
 import os
@@ -11,10 +10,8 @@ def new_doc(subject, name):
     loc = (Path.home() / "Documents" / "SchoolWork" / subject).absolute().resolve()
     file = (loc / f"{date.today().isoformat()}-{name}.docx").absolute().resolve()
     doc = Document(os.fspath(template))
-    date_para = doc.add_paragraph(date.today().strftime("%a, %d %b %Y"), "Heading2")
-    date_para.alignment = P_ALIGN.RIGHT
-    head = doc.add_paragraph(name, "Title")
-    head.alignment = P_ALIGN.CENTER
-    body = doc.add_paragraph("This is the body...", "Body")
+    for para in doc.paragraphs:
+        para.text = para.text.format({'date': date.today().strftime("%a, %d %b %Y"), 'heading': name,
+                                      'body': 'Replace this...'})
     doc.save(os.fspath(file))
-    Popen(["start", file.path])
+    Popen(["start", os.fspath(file)])
