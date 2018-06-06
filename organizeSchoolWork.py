28ba0b67a2caf851008e961ea11a824b2819cf82
bluepython508 <bluepython508@gmail.com>
2018-06-06 15:29:39 +0100

    Remove close_fds, ad shell=True to win32

--- organizeSchoolWork.py
+++ organizeSchoolWork.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from docx import Document
 from datetime import date
 from subprocess import Popen
@@ -29,19 +31,19 @@ SUBJECTS = sorted(list(set(SUBJECTS)))
 
 def subprocess_args(include_stdout=True):
     # The following is true only on Windows.
-    # if hasattr(subprocess, "STARTUPINFO"):
+    if hasattr(subprocess, "STARTUPINFO"):
         # On Windows, subprocess calls will pop up a command window by default
         # when run from Pyinstaller with the ``--noconsole`` option. Avoid this
         # distraction.
-        # si = subprocess.STARTUPINFO()
-        # si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        si = subprocess.STARTUPINFO()
+        si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
         # Windows doesn't search the path by default. Pass it an environment so
         # it will.
         # env = os.environ
-    # else:
-        # si = None
-        # env = None
+    else:
+        si = None
 
+    env = None
     # ``subprocess.check_output`` doesn't allow specifying ``stdout``::
     #
     #   Traceback (most recent call last):
@@ -52,28 +54,28 @@ def subprocess_args(include_stdout=True):
     #   ValueError: stdout argument not allowed, it will be overridden.
     #
     # So, add it only if it's needed.
-    # if include_stdout:
-    #     ret = {"stdout": subprocess.PIPE}
-    # else:
-    #     ret = {}
+    if include_stdout:
+        ret = {"stdout": subprocess.PIPE}
+    else:
+        ret = {}
 
     # On Windows, running this from the binary produced by Pyinstaller
     # with the ``--noconsole`` option requires redirecting everything
     # (stdin, stdout, stderr) to avoid an OSError exception
     # "[Error 6] the handle is invalid."
-    # if sys.platform == "win32":
-        # ret["close_fds"] = True
+    if sys.platform == "win32":
+        ret["shell"] = True
         # pass
-    # ret.update(
-    #     {
-    #         "stdin": subprocess.PIPE,
-    #         "stderr": subprocess.PIPE,
-            # "startupinfo": si,
-            # "env": env,
-        # }
-    # )
-    # return ret
-    return {}
+    ret.update(
+        {
+            "stdin": subprocess.PIPE,
+            "stderr": subprocess.PIPE,
+            "startupinfo": si,
+            "env": env,
+        }
+    )
+    return ret
+    # return {}
 
 
 def new_doc(subject, name):
@@ -107,4 +109,4 @@ def new_doc(subject, name):
     print(utility, os.fspath(file))
     print(subprocess_args())
     # sh.Command(utility)(os.fspath(file))
-    Popen([utility, os.fspath(file)])
+    Popen([utility, os.fspath(file)], **subprocess_args())
